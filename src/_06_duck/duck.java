6c529f123b0e77585abbb9b5c0e2f47bf024fa99
league <league@imac-23.attlocal.net>
2019-11-20 19:59:49 -0800

    finished step 4

---
+++ src/_06_duck/duck.java
@@ -0,0 +1,18 @@
+package _06_duck;
+
+public class duck {
+	
+	duck(String favoriteFood, int numberOfFriends) {
+      	this.favoriteFood = favoriteFood;
+      	this.numberOfFriends = numberOfFriends;
+}
+	
+	int numberOfFriends;
+	String favoriteFood;
+	void quack() {
+	}
+	void waddle() {
+	
+
+}
+}
---
+++ src/_06_duck/duck_class_runner.java
@@ -0,0 +1,5 @@
+package _06_duck;
+
+public class duck_class_runner {
+
+}
